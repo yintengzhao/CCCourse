b19f06e6e20ebde258d1236a100d1c9f2e42fce5
zhaowenjian <zhaowenjian@github.com>
2016-12-08 21:40:32 +0800

    cc course0.1 logger

--- app/controllers/Error.php
+++ app/controllers/Error.php
@@ -4,17 +4,22 @@
  */
 class ErrorController extends Yaf_Controller_Abstract
 {
-	public function errorAction($exception = null)
-	{
-		Yaf_Dispatcher::getInstance()->disableView();
-		$code    = $exception->getCode();
-		$message = $exception->getMessage();
-		$url     = $this->_request->getRequestUri();
-		Log::write('[' . $code . '](' . $url . '):' . $message, 'ERROR');
-		$response['status'] = -10;
-		$response['info']   = ['code' => $code, 'msg' => '请求异常！', 'uri' => $url];
-		header('HTTP/1.1 ' . $code);
-		header('Content-type: application/json');
-		echo json_encode($response, JSON_UNESCAPED_UNICODE); //unicode不转码
-	}
+    public function errorAction($exception)
+    {
+        Yaf_Dispatcher::getInstance()->disableView();
+        $code = $exception->getCode()?:500;
+        $url  = $this->_request->getRequestUri();
+    
+        Logger::error('[exception: {code}]({url}): {exception}', array(
+            'code'     => $code,
+            'url'      => $url,
+            'exception'=> $exception->__toString(),
+        ));
+    
+        $response['status'] = -10;
+        $response['info']   = ['code' => $code, 'msg' => '请求异常！', 'uri' => $url];
+
+        header('Content-Type: application/json; charset=utf-8', true, $code);
+        echo json_encode($response, JSON_UNESCAPED_UNICODE); //unicode不转码
+    }
 }
